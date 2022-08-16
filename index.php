f92579fc3b0deb239451d329b900f14b73cf7887
A. Osama <Altimos@desktop.com>
2022-08-16 06:09:45 +0200

    Fix paths issue

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-include_once "/funds-manager-backend/core/Application.php";
+include_once dirname(__DIR__) . "\\funds-manager-backend\\core\\Application.php";
 
-define("ROOT", dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define("ROOT", dirname(__DIR__) . "\\funds-manager-backend" . DIRECTORY_SEPARATOR);
+
 define("MODEL", ROOT . "models" . DIRECTORY_SEPARATOR);
 define("DATA", ROOT . "data" . DIRECTORY_SEPARATOR);
 define("CONTROLLER", ROOT . "controllers" . DIRECTORY_SEPARATOR);
