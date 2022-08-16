1db92089f0c7203d66356178f9696e3e0ffebf14
A. Osama <Altimos@desktop.com>
2022-08-16 07:09:05 +0200

    Fixing paths

--- index.php
+++ index.php
@@ -3,11 +3,15 @@ ini_set('display_errors', 1);
 
 include_once "core" . DIRECTORY_SEPARATOR . "Application.php";
 
-define("ROOT", dirname(__DIR__) . DIRECTORY_SEPARATOR);
+// define("ROOT", dirname(__DIR__) . DIRECTORY_SEPARATOR);
 
-define("MODEL", ROOT . "models" . DIRECTORY_SEPARATOR);
-define("DATA", ROOT . "data" . DIRECTORY_SEPARATOR);
-define("CONTROLLER", ROOT . "controllers" . DIRECTORY_SEPARATOR);
+// define("MODEL", ROOT . "models" . DIRECTORY_SEPARATOR);
+// define("DATA", ROOT . "data" . DIRECTORY_SEPARATOR);
+// define("CONTROLLER", ROOT . "controllers" . DIRECTORY_SEPARATOR);
+
+define("MODEL",  "models" . DIRECTORY_SEPARATOR);
+define("DATA",  "data" . DIRECTORY_SEPARATOR);
+define("CONTROLLER",  "controllers" . DIRECTORY_SEPARATOR);
 
 
 new Application;
