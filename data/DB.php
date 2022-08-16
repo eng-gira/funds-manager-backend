cc2c8b5087166951d70baf566b4bb808e3a757ef
A. Osama <Altimos@desktop.com>
2022-08-16 05:41:53 +0200

    DB conn.

--- data/DB.php
+++ data/DB.php
@@ -3,14 +3,15 @@
 
 class DB
 {
-    private static $host = "";
-    private static $username = "";
-    private static $pw = "";
-    private static $dbName = "";
-
     protected static function connect()
     {
-        $conn = new \mysqli(self::$host, self::$username, self::$pw, self::$dbName);
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $username = $url["user"];
+        $pw = $url["pass"];
+        $dbName = substr($url["path"], 1);
+
+        $conn = new \mysqli($host, $username, $pw, $dbName);
 
 
         return $conn;
