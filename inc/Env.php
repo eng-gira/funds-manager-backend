71e23653a7000c2bdd7a730e8df816fc0f5893b9
A. Osama <Altimos@desktop.com>
2022-12-31 14:02:06 +0200

    Update Env::get

--- inc/Env.php
+++ inc/Env.php
@@ -2,6 +2,7 @@
 
 
 class Env {
+    private static array $envAssoc = [];
     private static bool $envSaved = false;
     private static string $envFilePath = '.' . DIRECTORY_SEPARATOR . '.env';
 
@@ -12,16 +13,17 @@ class Env {
         }
 
         // return getenv($key);
-        return apache_getenv($key);
+        // return apache_getenv($key);
+        return self::$envAssoc[$key];
     }
     
     private static function saveEnvFile($absFilePath) {
         $envArr = file($absFilePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        // foreach($envArr as $line) $envAssoc[explode('=', $line)[0]] = explode('=', $line)[1];
-        foreach($envArr as $line) {
-            // putenv(explode('=', $line)[0] . '=' . explode('=', $line)[1]);
-            apache_setenv(explode('=', $line)[0], explode('=', $line)[1]);
-        }
+        foreach($envArr as $line) self::$envAssoc[explode('=', $line)[0]] = explode('=', $line)[1];
+        // foreach($envArr as $line) {
+        //     // putenv(explode('=', $line)[0] . '=' . explode('=', $line)[1]);
+        //     // apache_setenv(explode('=', $line)[0], explode('=', $line)[1]);
+        // }
 
         self::$envSaved = true;
     }
